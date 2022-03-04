e59839902974b6c4a7fef3a1609ef7f293f5aae1
ver <95198109+lv0314@users.noreply.github.com>
2022-03-04 23:27:50 +0900

    Revert "[ver] Step6 (#201)"
    This reverts commit b6c512b6893a4dd0e1873c78a12ba596f4edc307.

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import { getData } from "../data/sources.js";
-import { renderPageInit } from "./views/render.js";
-
-const main = () => {
-    getData("https://kakaopage-back.herokuapp.com/src")
-    .then(() => renderPageInit())
-}
-
-main();
